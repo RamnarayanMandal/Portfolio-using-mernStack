c0bbbd119ff5332d068824b6ac85dbb712d66a7b
RamnarayanMandal <ramnarayan847231@gmail.com>
2025-05-28 23:02:27 +0530

    added spnider

--- frontend/src/component/Portfolio.jsx
+++ frontend/src/component/Portfolio.jsx
@@ -1,108 +1,149 @@
-import React from 'react';
-import { Navbar } from './Navbar';
-import { Education } from './Eduction'; // Fixed the import typo from 'Eduction' to 'Education'
-import Skill from './skill/Skill';
-import { Project } from './Project';
-import { Footer } from './Footer';
-import { Profile } from './Profile';
-import ContactUs from './ContactUs';
-import WorkExperience from './WorkExperience';
-import { useTheme } from '../ThemeContext';
-import { Aboutme } from './Aboutme';
-import { CertificateComponent } from './CertificateComponent';
-import { BlogComponent } from './BlogComponent';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Navbar } from "./Navbar";
+import { Education } from "./Eduction";
+import Skill from "./skill/Skill";
+import { Project } from "./Project";
+import { Footer } from "./Footer";
+import { Profile } from "./Profile";
+import ContactUs from "./ContactUs";
+import WorkExperience from "./WorkExperience";
+import { useTheme } from "../ThemeContext";
+import { Aboutme } from "./Aboutme";
+import { CertificateComponent } from "./CertificateComponent";
+import { BlogComponent } from "./BlogComponent";
 import { Slide } from "react-awesome-reveal";
-import { useInView } from 'react-intersection-observer';
+import { useInView } from "react-intersection-observer";
+
+const URI = import.meta.env.VITE_API_URL;
 
 function Portfolio() {
-    const { isDarkMode } = useTheme();
+  const { isDarkMode } = useTheme();
+  const [userProfile, setUserProfile] = useState(null); // Initialize state
 
-    // Define dark and light mode styles
-    const containerClasses = isDarkMode
-        ? "font-sans text-gray-300 bg-gray-900"  // Dark mode styles
-        : "font-sans text-gray-800";              // Light mode styles without background
-
-    // Define light mode background style
-    const lightModeBackground = {
-        backgroundImage: 'url("https://img.freepik.com/free-photo/blue-toned-collection-paper-sheets-with-copy-space_23-2148320445.jpg?w=900&t=st=1709066598~exp=1709067198~hmac=c5c0995a7289d90e1e59f33310d419716d3975cedc8f97a8f31c119f7619dcaf")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundAttachment: 'fixed',
-        overflowX: 'hidden',
-        height: '100vh',
-        width: '100vw',
+  // Load user profile
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const response = await axios.get(`${URI}/api/users/getUser`);
+        console.log("Fetched data:", response.data.users);
+        setUserProfile(response.data.users);
+      } catch (error) {
+        console.error("Error fetching user profile:", error);
+      }
     };
 
-    // Custom Hook from react-intersection-observer
-    const { ref: profileRef, inView: profileInView } = useInView({ triggerOnce: true });
-    const { ref: aboutMeRef, inView: aboutMeInView } = useInView({ triggerOnce: true });
-    const { ref: workExperienceRef, inView: workExperienceInView } = useInView({ triggerOnce: true });
-    const { ref: educationRef, inView: educationInView } = useInView({ triggerOnce: true });
-    const { ref: skillRef, inView: skillInView } = useInView({ triggerOnce: true });
-    const { ref: projectRef, inView: projectInView } = useInView({ triggerOnce: true });
-    const { ref: certificateRef, inView: certificateInView } = useInView({ triggerOnce: true });
-    const { ref: blogRef, inView: blogInView } = useInView({ triggerOnce: true });
+    fetchUserProfile();
+  }, []);
 
-    return (
-        <div className={containerClasses} style={!isDarkMode ? lightModeBackground : {}}>
-            <Navbar />
+  const containerClasses = isDarkMode
+    ? "font-sans text-gray-300 bg-gray-900"
+    : "font-sans text-gray-800";
 
-            <main className="pt-[100px]">
-                {/* Wrap components with references to detect visibility */}
-                <Slide direction="left" triggerOnce when={profileInView}>
-                    <div ref={profileRef}>
-                        <Profile />
-                    </div>
-                </Slide>
+  const lightModeBackground = {
+    backgroundImage:
+      'url("https://img.freepik.com/free-photo/blue-toned-collection-paper-sheets-with-copy-space_23-2148320445.jpg?w=900&t=st=1709066598~exp=1709067198~hmac=c5c0995a7289d90e1e59f33310d419716d3975cedc8f97a8f31c119f7619dcaf")',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundAttachment: "fixed",
+    overflowX: "hidden",
+    height: "100%",
+    width: "100%",
+  };
 
-                <Slide direction="right" triggerOnce when={aboutMeInView}>
-                    <div ref={aboutMeRef}>
-                        <Aboutme />
-                    </div>
-                </Slide>
+  const { ref: profileRef, inView: profileInView } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: aboutMeRef, inView: aboutMeInView } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: workExperienceRef, inView: workExperienceInView } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: educationRef, inView: educationInView } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: skillRef, inView: skillInView } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: projectRef, inView: projectInView } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: certificateRef, inView: certificateInView } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: blogRef, inView: blogInView } = useInView({ triggerOnce: true });
 
-                <Slide direction="left" triggerOnce when={workExperienceInView}>
-                    <div ref={workExperienceRef}>
-                        <WorkExperience />
-                    </div>
-                </Slide>
+  // Show loader if profile data is not yet available
+  if (!userProfile) {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-gray-100 dark:bg-gray-900 animate-fadeIn">
+      <div className="w-16 h-16 border-4 border-indigo-600 dark:border-white border-t-transparent rounded-full animate-spin"></div>
+      <p className="mt-4 text-gray-800 dark:text-gray-200 text-lg animate-pulse">Loading...</p>
+    </div>
+  );
+}
 
-                <Slide direction="right" triggerOnce when={educationInView}>
-                    <div ref={educationRef}>
-                        <Education />
-                    </div>
-                </Slide>
 
-                <Slide direction="left" triggerOnce when={skillInView}>
-                    <div ref={skillRef}>
-                        <Skill />
-                    </div>
-                </Slide>
+  return (
+    <div
+      className={containerClasses}
+      style={!isDarkMode ? lightModeBackground : {}}
+    >
+      <Navbar />
+      <main className="pt-[100px]">
+        <Slide direction="left" triggerOnce when={profileInView}>
+          <div ref={profileRef}>
+            <Profile user={userProfile} />
+          </div>
+        </Slide>
 
-                <Slide direction="right" triggerOnce when={projectInView}>
-                    <div ref={projectRef}>
-                        <Project />
-                    </div>
-                </Slide>
+        <Slide direction="right" triggerOnce when={aboutMeInView}>
+          <div ref={aboutMeRef}>
+            <Aboutme />
+          </div>
+        </Slide>
 
-                <Slide direction="left" triggerOnce when={certificateInView}>
-                    <div ref={certificateRef}>
-                        <CertificateComponent />
-                    </div>
-                </Slide>
+        <Slide direction="left" triggerOnce when={workExperienceInView}>
+          <div ref={workExperienceRef}>
+            <WorkExperience />
+          </div>
+        </Slide>
 
-                <Slide direction="right" triggerOnce when={blogInView}>
-                    <div ref={blogRef}>
-                        <BlogComponent />
-                    </div>
-                </Slide>
-            </main>
+        <Slide direction="right" triggerOnce when={educationInView}>
+          <div ref={educationRef}>
+            <Education />
+          </div>
+        </Slide>
 
-            <ContactUs />
+        <Slide direction="left" triggerOnce when={skillInView}>
+          <div ref={skillRef}>
+            <Skill />
+          </div>
+        </Slide>
 
-            <Footer />
-        </div>
-    );
+        <Slide direction="right" triggerOnce when={projectInView}>
+          <div ref={projectRef}>
+            <Project />
+          </div>
+        </Slide>
+
+        <Slide direction="left" triggerOnce when={certificateInView}>
+          <div ref={certificateRef}>
+            <CertificateComponent />
+          </div>
+        </Slide>
+
+        <Slide direction="right" triggerOnce when={blogInView}>
+          <div ref={blogRef}>
+            <BlogComponent />
+          </div>
+        </Slide>
+      </main>
+      <ContactUs />
+      <Footer />
+    </div>
+  );
 }
 
 export default Portfolio;
