18f806332dcaa4c929667980e83fc3998e80475b
Ramnarayan Mandal <154451608+RamnarayanMandal@users.noreply.github.com>
2025-07-14 01:27:54 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -44,6 +44,7 @@ app.use('/api/blogs', BlogRoutes);
 
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port number ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port number ${PORT}`);
 });
+
