49fb7f958c9a468dc7c8bb36caf3efdf80c1a572
RamnarayanMandal <ramnarayan847231@gmail.com>
2025-07-20 03:20:01 +0530

    fixed the erreo

--- Backend/src/routes/projectRoute.js
+++ Backend/src/routes/projectRoute.js
@@ -1,18 +1,18 @@
 import express from 'express';
-import { createProject,getAllProjects, getProjectById, updateProject, deleteProject } from '../controller/projectController.js';
+import { createProject, getAllProjects, getProjectById, updateProject, deleteProject } from '../controller/projectController.js';
 import { upload } from '../middleware/multerMiddleware.js';
 
 const router = express.Router();
 
 // Route to create a new project
-router.post('/create', upload.fields([{ name: 'imageUrl', maxCount: 100 }]), createProject);
+router.post('/create', upload.fields([{ name: 'imageUrl', maxCount: 10 }]), createProject);
 
 // Route to get a project by ID
 router.get('/getById/:id', getProjectById);
 router.get('/getAll/', getAllProjects);
 
 // Route to update a project by ID
-router.put('/update/:id',upload.fields([{ name: 'imageUrl', maxCount: 100 }]), updateProject);
+router.put('/update/:id', upload.fields([{ name: 'imageUrl', maxCount: 100 }]), updateProject);
 
 // Route to delete a project by ID
 router.delete('/delete/:id', deleteProject);
